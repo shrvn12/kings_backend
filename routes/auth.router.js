68065cc3909cd8890d59660ab9f068514c9eb735
shrvn12 <singhshravan1208@gmail.com>
2026-01-01 22:09:53 +0530

    minor fix

--- routes/auth.router.js
+++ routes/auth.router.js
@@ -72,7 +72,7 @@ authRouter.post('/login', validateFields(["password"]), verifyPassword, async(re
         res.cookie('token', token, {
             httpOnly: true,
             secure: process.env.NODE_ENV === 'production',
-            sameSite: 'Strict',
+            sameSite: 'None',
             maxAge: 7 * 24 * 60 * 60 * 1000
         });
         res.status(200).send({msg: 'login successful', token});
